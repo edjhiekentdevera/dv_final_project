1e09cdfa12bcfcf3e0619b22405a9c808fc4eca8
edjhiekentdevera <edjhiekentdevera@gmail.com>
2026-07-14 13:31:52 +0800

    master

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Pipeline;
+use Illuminate\View\View;
+use Laravel\Fortify\Actions\AttemptToAuthenticate;
+use Laravel\Fortify\Actions\CanonicalizeUsername;
+use Laravel\Fortify\Actions\EnsureLoginIsNotThrottled;
+use Laravel\Fortify\Actions\PrepareAuthenticatedSession;
+use Laravel\Fortify\Contracts\RedirectsIfTwoFactorAuthenticatable;
+use Laravel\Fortify\Features;
+use Laravel\Fortify\Http\Requests\LoginRequest;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+
+    public function __construct(
+        protected StatefulGuard $guard,
+    ){}
+
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request)
+    {
+        return $this->loginPipeline($request)->then(function($request) {
+            return redirect()->intended(route('dashboard', absolute:false));
+        });
+        $request->authenticate(); 
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('dashboard', absolute: false));
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+    protected function loginPipeline(LoginRequest $request): Pipeline
+    {
+        return (new Pipeline(app()))
+            ->send($request)
+            ->through(array_filter([
+                config('fortify.limiters.login')? null: EnsureLoginIsNotThrottled::class,
+                config('fortify.lowercase_usernames')? CanonicalizeUsername::class: null,
+                Features::enabled(Features::twoFactorAuthentication())? RedirectsIfTwoFactorAuthenticatable::class: null,
+                AttemptToAuthenticate::class,
+                PrepareAuthenticatedSession::class,
+            ]));
+    }
+}
